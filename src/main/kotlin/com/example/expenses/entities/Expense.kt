5611e2b86f90a06ef318490435e5d8fe89460cf5
Rafael Guimarães <rafael.guimaraes@sequra.es>
2023-08-19 19:05:46 +0200

    Adding createdAt and updatedAt fields to Expense entity

--- src/main/kotlin/com/example/expenses/entities/Expense.kt
+++ src/main/kotlin/com/example/expenses/entities/Expense.kt
@@ -5,6 +5,9 @@ import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.Table
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import java.time.Instant
 import java.util.UUID
 
 @Entity
@@ -13,5 +16,9 @@ class Expense(
     @Id @GeneratedValue(strategy = GenerationType.UUID)
     var id: UUID = UUID.randomUUID(),
     var amount: Double,
-    var description: String?
+    var description: String?,
+    @CreatedDate
+    var createdAt: Instant = Instant.now(),
+    @LastModifiedDate
+    var updatedAt: Instant = Instant.now()
 )
